54e84834e0ce7e5bd92d3ab564df2091c51bdc38
sridharpanigrahi1 <25cse036.sridharpanigrahi@giet.edu>
2026-07-25 01:01:54 +0530

    Updated test2.java

--- test2.java
+++ test2.java
@@ -1,3 +1,30 @@
-public class test2 {
-    
+import java.util.Scanner;
+
+class test2
+{
+    int num, rem, sum;
+
+    public void cal()
+    {
+        System.out.println("Enter a number");
+
+        Scanner sc = new Scanner(System.in);
+
+        num = sc.nextInt();
+
+        while (num > 0)
+        {
+            rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
+        }
+
+        System.out.println("The sum of number is: " + sum);
+    }
+
+    public static void main(String args[])
+    {
+        test2 ob = new test2();
+        ob.cal();
+    }
 }
