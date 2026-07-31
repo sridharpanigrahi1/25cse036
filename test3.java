62a962189cb33935bd6e4152db1271349e41d385
sridharpanigrahi1 <25cse036.sridharpanigrahi@giet.edu>
2026-08-01 00:38:41 +0530

    palindrome check in Test3 class
    Added functionality to check if a number is a palindrome.

--- test3.java
+++ test3.java
@@ -1,3 +1,28 @@
-public class teat3 {
-    
+import java.util.Scanner;
+
+class Test3 {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+
+        sc.close();
+    }
 }
